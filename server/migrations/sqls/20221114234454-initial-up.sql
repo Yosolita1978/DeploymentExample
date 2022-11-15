49b6db146720099a6f70e04a0d3f6b3e72a564d1
Cristina Rodriguez <yosola@gmail.com>
2022-11-14 16:35:09 -0800

    Fixing tables names

--- server/migrations/sqls/20221114234454-initial-up.sql
+++ server/migrations/sqls/20221114234454-initial-up.sql
@@ -1,12 +1,12 @@
 /* Replace with your SQL commands */
 
-CREATE TABLE public.students (
+CREATE TABLE students (
     id SERIAL PRIMARY KEY,
     lastname character varying(255),
     firstname character varying(255)
 );
 
-CREATE TABLE public.users (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     lastname character varying(255),
     firstname character varying(255),
@@ -14,18 +14,18 @@ CREATE TABLE public.users (
     sub character varying(255)
 );
 
-INSERT INTO public.students (id, lastname, firstname) VALUES (1, 'Gomez', 'Andrea');
-INSERT INTO public.students (id, lastname, firstname) VALUES (2, 'Lee', 'Becca');
-INSERT INTO public.students (id, lastname, firstname) VALUES (3, 'Smith', 'Will');
-INSERT INTO public.students (id, lastname, firstname) VALUES (4, 'Pond', 'Kate');
-INSERT INTO public.students (id, lastname, firstname) VALUES (5, 'Lasso', 'Jamie');
-INSERT INTO public.students (id, lastname, firstname) VALUES (7, 'Rodriguez', 'Arepa');
-INSERT INTO public.students (id, lastname, firstname) VALUES (8, 'Smith', 'Crush');
-INSERT INTO public.students (id, lastname, firstname) VALUES (9, 'Fonca', 'Billy');
-INSERT INTO public.students (id, lastname, firstname) VALUES (10, 'Gomez', 'Teresa');
-INSERT INTO public.students (id, lastname, firstname) VALUES (11, 'Tres', 'Prueba');
-INSERT INTO public.students (id, lastname, firstname) VALUES (12, 'Cuatro', 'Prueba');
-INSERT INTO public.students (id, lastname, firstname) VALUES (13, 'Cinco', 'Prueba');
+INSERT INTO students (id, lastname, firstname) VALUES (1, 'Gomez', 'Andrea');
+INSERT INTO students (id, lastname, firstname) VALUES (2, 'Lee', 'Becca');
+INSERT INTO students (id, lastname, firstname) VALUES (3, 'Smith', 'Will');
+INSERT INTO students (id, lastname, firstname) VALUES (4, 'Pond', 'Kate');
+INSERT INTO students (id, lastname, firstname) VALUES (5, 'Lasso', 'Jamie');
+INSERT INTO students (id, lastname, firstname) VALUES (7, 'Rodriguez', 'Arepa');
+INSERT INTO students (id, lastname, firstname) VALUES (8, 'Smith', 'Crush');
+INSERT INTO students (id, lastname, firstname) VALUES (9, 'Fonca', 'Billy');
+INSERT INTO students (id, lastname, firstname) VALUES (10, 'Gomez', 'Teresa');
+INSERT INTO students (id, lastname, firstname) VALUES (11, 'Tres', 'Prueba');
+INSERT INTO students (id, lastname, firstname) VALUES (12, 'Cuatro', 'Prueba');
+INSERT INTO students (id, lastname, firstname) VALUES (13, 'Cinco', 'Prueba');
 
-SELECT pg_catalog.setval('public.students_id_seq', 16, true);
+SELECT pg_catalog.setval('students_id_seq', 16, true);
 
